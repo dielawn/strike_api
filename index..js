a8c414c441e20ad7b9c4dfa65ada547996016656
dielawn <dielawnmerkill@gmail.com>
2024-08-25 09:52:38 -0600

    all strike api calls complete

--- index..js
+++ index..js
@@ -10,19 +10,14 @@ const newStrikeInvoice = async (description, currency, amount) => {
     }
     const correlationId = uuidv4();
     const formattedCurrency = currency.toUpperCase();
-    let formattedAmount;
-    if (currency === 'USD') {
-        formattedAmount = amount.toFixed(2)
-    } else {
-        formattedAmount = amount.toFixed(8)
-    }
+    
     const data = { 
         // universally identifiable, must be unique
         correlationId,
         description,
         amount: {
             currency: formattedCurrency,
-            amount: formattedAmount
+            amount
         }
      }
     try {
@@ -282,7 +277,273 @@ const strikeLightningQuote = async (sourceCurrency, lnInvoice) => {
          console.error('Error creating new lightning quote:', error.response?.data || error.message);
          throw error; 
      }
+};
+
+const strikeOnChainQuotes = async (amount, currency, btcAddress) => {
+    const formattedCurrency = currency.toUpperCase();
+    if (currency !== 'USD' && formattedCurrency !== 'BTC') {
+        throw new Error(`Invalid currency. Expecting "USD" or "BTC". Recieved ${currency}`)
+    }
+    let formattedAmount;
+    if (formattedCurrency === 'USD') {
+        formattedAmount = amount.toFixed(2)
+    } else {
+        formattedAmount = amount.toFixed(8)
+    }
+    const data = {
+        btcAddress,
+        amount: {
+            amount,
+            currency: formattedCurrency
+        }
+    }
+    try {
+        const response = await axios.post(`https://api.strike.me/v1/payment-quotes/onchain/tiers`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },  
+        })
+        const responseData = response.data
+        console.log('On chain quote:', responseData)
+        return responseData
+
+    } catch (error) {
+         console.error('Error creating on chain tx quote:', error.response?.data || error.message);
+         throw error; 
+     }
+};
+
+
+//Fee tiers 'tier_fast', 'tier_standard', 'tier_free'
+const onChainByFeeTier = async (amount, currency, btcAddress, tier, description) => {
+    if (tier !== 'tier_fast' && tier !== 'tier_standard' && tier !== 'tier_free') {
+        throw new Error(`Invalid fee tier. Expecting  'tier_fast', 'tier_standard' or 'tier_free'. Recieved ${tier}`)
+    }
+    const formattedCurrency = currency.toUpperCase();
+    if (currency !== 'USD' && formattedCurrency !== 'BTC') {
+        throw new Error(`Invalid currency. Expecting "USD" or "BTC". Recieved ${currency}`)
+    }
+    let formattedAmount;
+    if (formattedCurrency === 'USD') {
+        formattedAmount = amount.toFixed(2)
+    } else {
+        formattedAmount = amount.toFixed(8)
+    }
+    const data = {
+        btcAddress,
+        sourceCurrency: formattedCurrency,
+        description,
+        amount: {
+            amount: formattedAmount,
+            currency: formattedCurrency,
+            feePolicy: 'INCLUSIVE'
+        },
+        onchainTierId: tier
+    }
+    try {
+        const response = await axios.post(`https://api.strike.me/v1/payment-quotes/onchain`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },  
+        })
+        const responseData = response.data
+        console.log('On chain quote:', responseData)
+        return responseData
+
+    } catch (error) {
+         console.error('Error creating on chain tx quote:', error.response?.data || error.message);
+         throw error; 
+     }
+};
+
+//Execute payment
+const executeStrikePayment = async (quoteId) => {
+    try {
+        const response = await axios.patch(`https://api.strike.me/v1/payment-quotes/${quoteId}/execute`, null, { 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },    
+        });
+        const responseData = response.data;
+        console.log('Payment executed:', responseData);
+        return responseData;
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+};
+
+
+const getPaymentStateById = async (paymentId) => {
+    try {
+        const response = await axios.get(`https://api.strike.me/v1/payments/${paymentId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`,
+            },
+        });
+        const responseData = response.data
+        console.log('Payment by id:', responseData)
+        return responseData
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
 }
+
+// Directing payments
+//Creating an invoice on behalf of a Strike user
+const invFromHandle = async (handle, description, currency, amount) => {
+    if (!handle || !description || !currency || amount === undefined) {
+        throw new Error('Missing required parameters');
+    }
+    const correlationId = uuidv4();
+    const formattedCurrency = currency.toUpperCase();
+    const data = {
+        correlationId,
+        description,
+        amount: {
+            currency: formattedCurrency,
+            amount
+        }
+    }
+    try {   
+        const response = await axios.post(`https://api.strike.me/v1/invoices/handle/${handle}`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },     
+        })
+        const responseData = response.data
+        console.log('Inv from handle: ', responseData)
+        return responseData
+
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+};
+
+const payoutToBank = async () => {
+    const data = {
+        transferType: 'ACH',
+        accountNumber: '1234567890',
+        routingNumber: '123456789',
+        accountType: 'CHECKING',
+        bankName: 'Chase Bank',
+        bankAddress: {
+          country: 'US',
+          state: 'TX',
+          city: 'Austin',
+          postCode: '73301',
+          line1: '123 Main St'
+        },
+        beneficiaries: [
+          {
+            dateOfBirth: '1980-03-02',
+            type: 'INDIVIDUAL',
+            name: 'John Smith',
+            address: {
+              country: 'US',
+              state: 'TX',
+              city: 'Austin',
+              postCode: '73301',
+              line1: '123 Main St'
+            }
+          },
+          {
+            email: 'AliceSmith@example.com',
+            phoneNumber: '+15551234567',
+            url: 'https://example.com',
+            type: 'INDIVIDUAL',
+            name: 'Alice Smith',
+            address: {
+              country: 'US',
+              state: 'TX',
+              city: 'Austin',
+              postCode: '73301',
+              line1: '123 Main St'
+            }
+          }
+        ]
+      }
+    try {
+        const response = await axios.post(`https://api.strike.me/v1/payment-methods/bank`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },   
+        })
+        const responseData = response.data
+        console.log('Bank payout request', responseData)
+        return responseData
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+};
+
+// Currency exchange
+const exchangeCurrencyQuote = async (sellCurrency, buyCurrency, amount) => {
+    const formattedSellCurrency = sellCurrency.toUpperCase()
+    const formattedBuyCurrency = buyCurrency.toUpperCase()
+    const data = {
+        sell: formattedSellCurrency,
+        buy: formattedBuyCurrency,
+        amount: {
+            amount,
+            currency: formattedSellCurrency
+        }
+    }
+    try {
+        const response = await axios.post('https://api.strike.me/v1/currency-exchange-quotes', data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },   
+        })
+        const responseData = response.data
+        console.log('exchange quote', responseData)
+        return responseData
+
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+};
+
+const executeCurrencyExchange = async (quoteId) => {
+    try {
+        const response = await axios.patch(`https://api.strike.me/v1/currency-exchange-quotes/${quoteId}/execute`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },   
+        })
+        const responseData = response.data
+        console.log('Executed exchange', responseData)
+        return responseData
+
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+};
+
+const checkQuoteStatus = async (quoteId) => {
+    try {
+        const response = await axios.get(`https://api.strike.me/v1/currency-exchange-quotes/${quoteId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.STRIKE_API_KEY}`, 
+            },   
+        })
+        const responseData = response.data
+        console.log('quote status', responseData)
+        return responseData
+        
+    } catch (error) {
+        console.error('Error ', error.response?.data || error.message);
+    }
+}
+
 
 module.exports = {
     newStrikeInvoice, 
@@ -292,5 +553,10 @@ module.exports = {
     newStrikeQuote,
     checkStrikePaymentStatus,
     allInvoiceOf,
-    strikeLightningQuote
+    strikeLightningQuote,
+    strikeOnChainQuotes,
+    onChainByFeeTier,
+    executeStrikePayment,
+    getPaymentStateById,
+    invFromHandle
 }
--- test-invoice.js
+++ test-invoice.js
@@ -7,13 +7,18 @@ const {
   newStrikeQuote,
   checkStrikePaymentStatus,
   allInvoiceOf,
-  strikeLightningQuote
+  strikeLightningQuote,
+  strikeOnChainQuotes,
+  onChainByFeeTier,
+  executeStrikePayment,
+  getPaymentStateById,
+  invFromHandle
 } = require('./index.');
 
 // const invoiceId = '7e653b7b-de1b-4ce6-a9ad-a4ce22a1a8ca'
 // const quoteId = 'c809943a-8d73-4f94-830e-d09f7e591629'
-// const lightningInv = ''
-
+const lightningInv = 'lnbc690n1pnv5w23pp57j6296xvj5n4vgdylr8cy3tddnlrkm3skpxkksqtlqu27a4hdhcsdq5w3jhxapqd9h8vmmfvdjscqzzsxqrrs0sp5zc7j6wxzaayec0xzr6fvjglaw8qmg8keqepd0jw3s20p4xv0tzrq9qyyssq7p8ehrhws26jpjng2m3k68xts0e5j8e7946a0rjc97gl3nh82al4e3e6dlmwu0pz54tp2urv4f40xc2fxve8pj8jglhqr33zrx0gkwcqyx9qx5'
+const btcAddress = process.env.STRIKE_ONCHAIN_ADDRESS
 async function testNewInvoice(description, currency, amount) {
   const formattedCurrency = currency.toUpperCase()
   if (formattedCurrency !== 'BTC' && formattedCurrency !== 'USD') {
@@ -32,7 +37,7 @@ async function testNewInvoice(description, currency, amount) {
   }
 };
 
-// testNewInvoice('test invoice', 'BTC' .0005476);
+// testNewInvoice('test invoice', 'BTC', .00000069);
 
 async function testGetProfile(handle) {
     try {
@@ -151,7 +156,7 @@ async function testAllInvOf(status) {
 
 // testAllInvOf('unpaid')
 
-async function testPayStrikeLightning( lnInvoice) {
+async function testPayStrikeLightning(lnInvoice) {
   try {
     const response = await strikeLightningQuote('btc', lnInvoice)
     const responseData = response.data
@@ -167,16 +172,145 @@ async function testPayStrikeLightning( lnInvoice) {
   }
 };
 
-// testPayStrikeLightning(lightningInv)
+// testPayStrikeLightning('lnbc690n1pnv50qfsp5enda6qd5zukc67600rvxyd5sc24el3l64mmh7sfwmpqms5gc6rpspp5e2j79mw69s60vmy4h4hlas88srvt5v303qr840kemupksffpc8asdqqnp4qwh05slmksqfkgdyz2wst9fewjmah2amldg3jg2pqzqgvr723mslqxqrrsxcqzzn9qyysgq2fxqlmx27zdwu2zmpgqpjm4smqe9d80h4e729mw9gq6xk0s65d0hjnzdyztudpdrenje06pfxqggvn7g0zh3aaf42fhsj36zvkduw2spz8p3qs')
+
 
 async function lnInvoice() {
-  const invoice = await testNewInvoice('test invoice', 'BTC', .00054)
+  const invoice = await testNewInvoice('test invoice', 'BTC', .00000069)
   console.log('invoice', invoice)
   const quote = await testNewQuote(invoice.invoiceId)
   console.log('quote', quote)
   const lightningInv = quote.lnInvoice
   console.log(lightningInv)
+  return lightningInv
 }
 
-lnInvoice()
-// testAllInvOf('unpaid')
+// lnInvoice()
+testAllInvOf('unpaid')
+testAllInvOf('paid')
+testAllInvOf('pending')
+
+//On chain quote
+async function testOnChainQuote(amount, currency, btcAddress) {
+  try {
+    const quote = await strikeOnChainQuotes(amount, currency, btcAddress)
+    console.log('On chain quote: ', quote)
+    return quote
+  } catch (error) {
+    if (error.name === "NetworkError") {
+      console.error(`Network error getting on chain quote`);
+      return null;
+    }
+    console.error(`Error getting on chain quote:`, error.message);
+    return null;
+  }
+};
+
+// testOnChainQuote(.005, 'btc', btcAddress)
+
+//Example response 
+// [
+//   {
+//     "id": "tier_fast",
+//     "estimatedDeliveryDurationInMin": 10,
+//     "estimatedFee": {
+//       "amount": "0.00002862",
+//       "currency": "BTC"
+//     }
+//   },
+//   {
+//     "id": "tier_standard",
+//     "estimatedDeliveryDurationInMin": 60,
+//     "estimatedFee": {
+//       "amount": "0.00000363",
+//       "currency": "BTC"
+//     }
+//   },
+//   {
+//     "id": "tier_free",
+//     "estimatedDeliveryDurationInMin": 720,
+//     "estimatedFee": {
+//       "amount": "0",
+//       "currency": "BTC"
+//     }
+//   }
+// ]
+
+//Quote by tier
+async function testFeeByTier(amount, currency, btcAddress, tier, description) {
+  try {
+    const tierQuote = await onChainByFeeTier(amount, currency, btcAddress, tier, description)
+    console.log(`Estimated delivery ${tierQuote.estimatedDeliveryDurationInMin} min. Fee: ${tierQuote.totalFee.amount} ${tierQuote.totalFee.currency}`)
+    return tierQuote
+  } catch (error) {
+    if (error.name === "NetworkError") {
+      console.error(`Network error getting on chain tier quote`);
+      return null;
+    }
+    console.error(`Error getting on chain tier quote:`, error.message);
+    return null;
+  }
+};
+const aquaAddress = process.env.AQUA_ONCHAIN_ADDRESS
+// testFeeByTier(.0069, 'btc', aquaAddress, 'tier_free', 'free tier test')
+// testFeeByTier(.0069, 'btc', aquaAddress, 'tier_standard', 'standard fee quote test')
+// testFeeByTier(.0069, 'btc', aquaAddress, 'tier_fast', 'fast fee quote test')
+
+
+//Execute payment
+async function testExecutePayment(quoteId) {
+  try {
+    console.log('quoteId', quoteId)
+
+    const response = await executeStrikePayment(quoteId);
+    console.log('Executed payment', response)
+    return response
+  } catch (error) {
+    if (error.name === "NetworkError") {
+      console.error(`Network error executing payment`);
+      return null;
+    }
+    console.error(`Error executing payment:`, error.message);
+    return null;
+  }
+};
+
+// testExecutePayment(quoteId) 
+
+async function paymentState(paymentId) {
+ try {
+  const payment = await getPaymentStateById(paymentId)
+  console.log('Payment', payment)
+
+  const state = payment.state
+  console.log('Payment state: ', state)
+  return state
+
+ } catch (error) {
+  if (error.name === "NetworkError") {
+    console.error(`Network error getting payment state`);
+    return null;
+  }
+  console.error(`Error getting payment state:`, error.message);
+  return null;
+}
+};
+
+
+// Invoice from user handle
+async function  invFrom(handle, description, currency, amount) {
+  try {
+    const invoice = await invFromHandle(handle, description, currency, amount)
+    console.log(`User ${handle} invoice: `, invoice)
+    return invoice
+
+  } catch (error) {
+    if (error.name === "NetworkError") {
+      console.error(`Network error getting invoice from handle`);
+      return null;
+    }
+    console.error(`Error egetting invoice from handle:`, error.message);
+    return null;
+  }
+};
+invFrom('dmercill', 'test inv from handle', 'BTC', .00000069)
